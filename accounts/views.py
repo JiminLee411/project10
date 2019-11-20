85a23e79271fedf828643c01991466e3c393569a
JiminLee411 <wlals41189@gmail.com>
2019-11-20 15:34:49 +0900

    LJM | base.html + signup(ing)

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 # Create your views here.
+def signup(request):
+    form = CustomUserCreationForm
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
