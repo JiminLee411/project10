43af55edd6143da643487880d5767d37f96b81b1
JiminLee411 <wlals41189@gmail.com>
2019-11-22 01:10:59 +0900

    LJM |  views.py(signup, index, detail)

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
+def index(request):
+    context = {
+        'users' : get_user_model().objects.all()
+    }
+    return render(request, 'accounts/index.html', context)
+
 def signup(request):
-    form = CustomUserCreationForm
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            auth_login(request, form.save())
+            return redirect('accounts:index')
+    else:
+        form = CustomUserCreationForm
     context = {
         'form': form
     }
     return render(request, 'accounts/form.html', context)
 
+def detail(request, user_pk):
+    context = {
+        'user_profile' : get_user_model().objects.get(pk=user_pk)
+    }
+    return render(request, 'accounts/detail.html', context)
+
 def login(request):
     pass
 
 def logout(request):
+    pass
+
+def follow(request, user_pk):
     pass
