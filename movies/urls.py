824757832c46d6c67c32789f8d85bb0a42bbeb3d
dmswjd9501 <dmswjd950101@gmail.com>
2019-11-22 01:17:55 +0900

    KEJ | movies templates, forms, urls, views(진행중)

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'movies'
+
 urlpatterns = [
     path('movies/', views.movies_index, name='movies_index'),
     path('movies/<int:movie_pk>/', views.movies_detail, name='movies_detail'),
 
-    path('movies/<int:movie_pk>/reviews/', views.review_create, name='review_create'),
+    path('movies/<int:movie_pk>/reviews/new/', views.review_create, name='review_create'),
     path('reviews/<int:review_pk>/', views.review_update_delete, name='review_update_delete'),
 ]
