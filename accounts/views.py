a60e74899d9f23cab02a47b0af2d69b2d3d8a091
JiminLee411 <wlals41189@gmail.com>
2019-11-22 09:34:30 +0900

    LJM | Accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def index(request):
     context = {
@@ -31,11 +32,30 @@ def detail(request, user_pk):
     return render(request, 'accounts/detail.html', context)
 
 def login(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect('accounts:index')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
 
 def logout(request):
     auth_logout(request)
     return redirect('accounts:index')
 
+@login_required
 def follow(request, user_pk):
-    pass
+    user = get_object_or_404(get_user_model(), pk=user_pk)
+    if request.user != user:
+        if user in request.user.followers.all():
+            request.user.followers.remove(user)
+        else:
+            request.user.followers.add(user)
+    return redirect('accounts:detail', user_pk)
